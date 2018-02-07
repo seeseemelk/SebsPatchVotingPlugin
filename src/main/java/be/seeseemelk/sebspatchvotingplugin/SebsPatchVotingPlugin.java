b8d05a5129f1887e5aefe413865e853e414b72c7
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2018-02-07 21:40:30 +0100

    Fix unnecessary default clause

--- src/main/java/be/seeseemelk/sebspatchvotingplugin/SebsPatchVotingPlugin.java
+++ src/main/java/be/seeseemelk/sebspatchvotingplugin/SebsPatchVotingPlugin.java
@@ -125,8 +125,6 @@ public class SebsPatchVotingPlugin extends JavaPlugin implements Listener
 					opened = false;
 					Bukkit.broadcastMessage(Messages.MSG_CLOSED);
 					break;
-				default:
-					return false;
 			}
 		}
 		else
